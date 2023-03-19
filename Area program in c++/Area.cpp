4e809de31ec204420a98e602609c3c6c89bf756c
Samuel Oddoye <addoyesamuel@gmail.com>
2023-03-19 09:45:23 +0000

    Add files via upload
    This is a program to calculate the area of shapes.
    I used C++ to create this app.
    It has no GUI thou, it's a command line program

---
+++ Area program in c++/Area.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    // this program accepts the first input to be the lenght, the second to be the width and diplay the area with the formula 
+    //Area=Length*width
+    cout<< "Type in the length below" << endl;
+    int length;
+    cin >> length<< endl;
+    Cout<< "Type in the width below" << endl;
+    int width;
+    cin >> width << endl;
+    int Area;
+    Area=Length*width;
+    Cout << Area;
+    
+    return 0;
+}
---
+++ Area program in c++/Personal Stuff/Main.c++
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string phraseentry_;
+    cout << "Hello World" << " " << " :"; 
+    cin >> phraseentry_; 
+    cout << "end_it";
+    return 0;
+}
