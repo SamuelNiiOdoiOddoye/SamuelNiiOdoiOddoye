2bf65e610aef8427455efd67d57c1b540a11dfbd
Samuel Oddoye <addoyesamuel@gmail.com>
2023-03-19 10:07:40 +0000

    Add files via upload
    another encryption app in python

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
-# write a function in python that accepts 2 string parameters.
-# The first parameter will be a string of characters, and the second parameter will be the same string of characters,
-# but they'll be in a different order and have one extra character.
-# For example if the first parameter is "eueiieo" and the second is "ieoedue" then the function should return "d"
+# take a message from the user
+# convert the input into characters only
+# display the ascii equivalence of the characters onto the console
 
-# create a function to take in two inputs of the same data type string
-# shuffle the elements of the second variable and then print out the difference
-def Spot_The_Difference():
-    #this point of the function is to accept 2 inputs from the user
-    #this is where two variables are declared and assigned to hold to the entries
-    First_Entry = input("Type in your first word here: ")
-    Second_Entry =input("Type in your second entry here: ")
+# this is the point of the code where the user inputs his ir hr message into the console
+# the variable User_Input_1 holds the message the user inputs into the console to be encrypted
+User_Input_1 = input("Type in Your desired message here: ")
 
-    #creating a variable to hold the splitted elements of the First_Entry and Second_Entry
-    First_Entry_Splitted = First_Entry.split()
-    Second_Entry_Splitted = Second_Entry.split()
-    First_Entry_Sum = 0
-    Second_Entry_Sum = 0
+#this part of the code holds words in the sentence
+# a variable l1 is to hold the words
+l1 = User_Input_1.split()
 
-# a for loop to change every character in First_Entry to it's ASCII equivalvent and sum up all the ASCII equivalent's
-    for c in First_Entry:
-        First_Entry_Sum += int(ord(c))
+# a for loop to populate an array of data type character with the message the user inputs into the console
+# c in the for loop below represents every character in the variable User_Input_1
+# This portion was originally meant to present every letter in a word
+# but in the case of the user input being a sentence it still prints all individual characters
+for c in User_Input_1:
+   Y = print(ord(c))
 
-# a for loop to change every character in Second_Entry to it's ASCII equivalent and sum up all the ASCII equivalent's
-    for c in Second_Entry:
-        Second_Entry_Sum += int(ord(c))
 
-# an if condition to subtract the sum of the second entry from the first entry if the first entry sum is greater than
-# the second entry sum
-    if First_Entry_Sum > Second_Entry_Sum:
-        l1 = First_Entry_Sum - Second_Entry_Sum
-        print(chr(l1), "is the unique entry of the two entries and is found in", First_Entry)
+# an attempt to save the characters into a variable and then print out the characters
+# Ask the user if he wants to print the original word or sentence
+# a variable User_Response to hold user answer
+# declare an if....else statement for if the user wants to or to not print his original entry
 
-# an if condition to subtract the sum of the first entry from the second entry if the second entry sum is greater than
-# the first entry sum
-    if Second_Entry_Sum > First_Entry_Sum:
-        l2 = Second_Entry_Sum - First_Entry_Sum
-        print(chr(l2), "is the unique entry of the two entries and is found in", Second_Entry)
-
-# an if condition if both entries have the same ASCII sum
-    if Second_Entry_Sum == First_Entry_Sum:
-        print("There is no unique character in both entries")
-
-Spot_The_Difference()
+print(l1)
+print(User_Input_1)
